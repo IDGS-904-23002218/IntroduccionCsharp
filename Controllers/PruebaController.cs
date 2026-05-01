0947b8b46949b70de455c8d8083f8b0ce2e6d435
Juan Carlos López <cr1757579@gmail.com>
2026-04-30 19:51:04 -0600

    Introduccion Csharp

---
+++ Controllers/PruebaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCsharp.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Suma(string x, string y, string r1)
+        {
+            int res = Convert.ToInt16(x) + Convert.ToInt16(y);
+            ViewBag.Res = Convert.ToString(res);
+
+
+            return View();
+        }
+    }
+}
