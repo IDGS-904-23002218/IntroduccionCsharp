3a6c424b3731024017ebe6527f225964c0621047
Juan Carlos López <cr1757579@gmail.com>
2026-05-06 20:57:45 -0600

    Modelos 06/05/2026

---
+++ Models/Practica1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploCsharp
+{
+    internal class Practica1
+    {
+        static void Main(String[] args)
+        {
+            // int, string, bool, double, char
+            int cal1 = 0;
+            int cal2, cal3, resultado;
+            string dato1;
+            Console.WriteLine("Ingresa primera calificación: ");
+            dato1 = Console.ReadLine();
+            cal1 = Convert.ToInt32(dato1);
+            Console.WriteLine("Ingresa segunda calificación: ");
+            dato1 = Console.ReadLine();
+            cal2 = Convert.ToInt32(dato1);
+
+            int suma = cal1 + cal2;
+
+            Console.WriteLine("La suma de las calificaciones es: " + suma);
+
+            //if-else
+            if (cal1 > cal2)
+            {
+                Console.WriteLine("La nota mayor es: " + cal1);
+            }
+            else
+            {
+                Console.WriteLine("La nota mayor es: " + cal1);
+            }
+
+            //while
+            int x;
+            x = 1;
+            while (x <= 100)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x++;
+            }
+
+            // do-while
+            int y = 1;
+            do
+            {
+                Console.Write(y);
+                Console.Write(" - ");
+                y++;
+            } while (y <= 100);
+
+            // for
+            int f;
+            for (f = 1; f <= 100; f++)
+            {
+                Console.Write(f);
+                Console.Write(" - ");
+            }
+        }
+    }
+}
---
+++ Models/Practica2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploCsharp
+{
+    class Practica2
+    {
+        String nombre;
+        private int edad;
+        protected string carrera;
+
+        public void IngresarDatos()
+        {
+            Console.Write("Ingresar tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresa tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa tu carrera: ");
+            carrera = Console.ReadLine();
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Carrera: " + carrera);
+        }
+
+        static void Main(String[] args)
+        {
+            Practica2 p2 = new Practica2();
+            p2.IngresarDatos();
+            p2.MostrarDatos();
+        }
+    }
+}
